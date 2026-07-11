1eac4e2688cbcfc99b63fe1c5705c5b25524e031
Sahil Hinge <sahilhinge89@gmail.com>
2026-07-11 14:24:58 +0530

    Remove jenkins docker run stage

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ http.createServer((req, res) => {
   res.end('Hello from Jenkins multibranch pipeline!\n');
 }).listen(3000);
 console.log('Server running on port 3000');// dev branch tweak
+// i have build this app 
